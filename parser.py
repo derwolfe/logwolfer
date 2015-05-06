a423d3f0e6599923387c2ae87754a0ece9f80a51
derwolfe <chriswwolfe@gmail.com>
2015-05-05 21:18:22 -0500

    tests for parser

--- parser.py
+++ parser.py
@@ -6,5 +6,30 @@ to load new records into the database and to analyze them.
 import json
 
 
+class Message(object):
+
+    def __init__(self,
+                 msg_id,
+                 recv_from,
+                 site_id,
+                 msg_type,
+                 status,
+                 timestamp):
+        self.msg_id = msg_id
+        self.recv_from = recv_from
+        self.site_id = site_id
+        self.msg_type = msg_type
+        self.status = status
+        self.timestamp = timestamp
+
+
 def parse_line(line):
-    return json.loads(line)
+    msg = json.loads(line)
+    return Message(
+        msg_id=msg["id"],
+        recv_from=msg["from"],
+        site_id=msg["site_id"],
+        msg_type=msg["type"],
+        status=msg["data"]["status"],
+        timestamp=msg["timestamp"]
+    )
--- test_parser.py
+++ test_parser.py
@@ -11,8 +11,8 @@ import parser
 
 class TestParser(TestCase):
 
-    def test_loadsJson(self):
-        msg = json.dumps({
+    def setUp(self):
+        self.msg = {
             "id":"53367bc7-e2cf-11e4-81da-56847afe9799",
             "from":"operator1",
             "site_id":"123",
@@ -20,10 +20,27 @@ class TestParser(TestCase):
             "data":{
                 "status":"online",
             },
-            "timestamp":1429026448,
-        })
-        result = parser.parse_line(msg)
-        self.assertIn("id", result.keys())
+            "timestamp":1429026448
+        }
+        self.result = parser.parse_line(json.dumps(self.msg))
+
+    def test_parsesMsgId(self):
+        self.assertEqual(self.msg["id"], self.result.msg_id)
+
+    def test_parsesRecvFrom(self):
+        self.assertEqual(self.msg["from"], self.result.recv_from)
+
+    def test_parsesSiteId(self):
+        self.assertEqual(self.msg["site_id"], self.result.site_id)
+
+    def test_parsesType(self):
+        self.assertEqual(self.msg["type"], self.result.msg_type)
+
+    def test_parsesStatus(self):
+        self.assertEqual(self.msg["data"]["status"], self.result.status)
+
+    def test_parsesTimestamp(self):
+        self.assertEqual(self.msg["timestamp"], self.result.timestamp)
 
 
 if __name__ == '__main__':
