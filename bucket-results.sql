10c564d0c3f498c77ec980b9469e06d3cf9b4767
derwolfe <chriswwolfe@gmail.com>
2015-05-10 22:12:03 -0500

    continuing to work on query, going for a join with idx

--- bucket-results.sql
+++ bucket-results.sql
@@ -1,4 +1,4 @@
-CREATE TEMP TABLE chats (
+CREATE TABLE chats (
   message_id TEXT NOT NULL
   , site_id TEXT NOT NULL
   , chat INTEGER NOT NULL
@@ -12,25 +12,37 @@ SELECT
     ( SELECT s.timestamp
       FROM Statuses s
       WHERE s.timestamp <= m.timestamp
-        and s.site_id = m.site_id
         and s.status == 1  -- online
+        and s.site_id = m.site_id
      ORDER BY s.timestamp DESC
      LIMIT 1) is null THEN 0 ELSE 1 END
      as chat
 FROM messages m;
 
+-- build an index on site_id
+CREATE INDEX chats_site_id_idx ON chats(site_id, chat);
+
+
 SELECT
   m.site_id AS site_id
-  , as chat
-  , as email
-  , (SELECT COUNT(*) FROM
-      (SELECT distinct FROM_id
-       FROM statuses st WHERE st.site_id = s.site_id))
-  , (SELECT COUNT(*) FROM
-       (SELECT DISTINCT FROM_id
-        FROM messages ms WHERE ms.site_id = s.site_id))
-FROM
-        messages m JOIN sites s
-                ON m.site_id = s.site_id
+   -- , COUNT(ch.chat)
+   -- , COUNT(email.chat)
+   , (SELECT COUNT(*) FROM
+       (SELECT distinct FROM_id
+        FROM statuses st WHERE st.site_id = s.site_id))
+   , (SELECT COUNT(*) FROM
+        (SELECT DISTINCT FROM_id
+         FROM messages ms WHERE ms.site_id = s.site_id))
+FROM messages m
+     JOIN sites s
+          ON m.site_id = s.site_id
+--      JOIN chats ch
+--           ON s.site_id = ch.site_id
+--      JOIN chats email
+--           ON s.site_id = ch1.site_id
+-- WHERE
+--       ch.chat = 1
+--       and email.chat = 0
 GROUP BY m.site_id
-ORDER BY m.site_id asc;
+ORDER BY m.site_id asc
+LIMIT 50;
