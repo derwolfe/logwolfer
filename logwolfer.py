6cd4c44ba8c864620ec8c38402ddcf26116566c7
derwolfe <chriswwolfe@gmail.com>
2015-05-11 22:08:29 -0500

    small doc fix

--- logwolfer.py
+++ logwolfer.py
@@ -1,6 +1,6 @@
 """
-The parser module contains all of the functions needed
-to load new records into the database and to analyze them.
+The logwolfer module contains all of the functions needed to load new records
+into the database and to analyze them.
 """
 
 from __future__ import absolute_import, print_function
